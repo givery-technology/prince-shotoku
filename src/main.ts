42c22e11894097c626860ccdbc0db658f8695358
takayukioda <takayuki@odashi.me>
2020-03-14 16:12:15 +0900

    chore: initial code

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { App } from '@slack/bolt';
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
+
+(async (): Promise<void> => {
+  await app.start(process.env.PORT || 3000);
+})();
