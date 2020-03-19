ce9c004923c64f747c98729567f66a072129af18
takayukioda <takayuki@odashi.me>
2020-03-20 02:00:08 +0900

    try: check if event works which did not

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,38 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
 });
 
+app.event('app_home_opened', async ({ event }) => {
+  console.log('app_home_opened', event);
+});
+app.event('channel_archive', async ({ event }) => {
+  console.log('channel_archive', event);
+});
+app.event('channel_created', async ({ event }) => {
+  console.log('channel_created', event);
+});
+app.event('channel_deleted', async ({ event }) => {
+  console.log('channel_deleted', event);
+});
+app.event('channel_left', async ({ event }) => {
+  console.log('channel_left', event);
+});
+app.event('channel_rename', async ({ event }) => {
+  console.log('channel_rename', event);
+});
+app.event('channel_shared', async ({ event }) => {
+  console.log('channel_shared', event);
+});
+app.event('channel_unarchive', async ({ event }) => {
+  console.log('channel_unarchive', event);
+});
+app.event('channel_unshared', async ({ event }) => {
+  console.log('channel_unshared', event);
+});
+app.event('message.channels', async ({ event }) => {
+  console.log('message.channels', event);
+});
+
 (async (): Promise<void> => {
   await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
 })();
