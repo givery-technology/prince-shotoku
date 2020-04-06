4d1e575f695b4ac498c765a41f55ba916ab22dee
takayukioda <takayuki@odashi.me>
2020-04-07 02:04:46 +0900

    feat: add messages on share

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import { App, LogLevel } from '@slack/bolt';
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
-  logLevel: LogLevel.DEBUG,
+  logLevel: LogLevel.INFO,
 });
 
-const OWNER_CHANNEL_ID='CQZPD378S'
+const OWNER_CHANNEL_ID=process.env.OWNER_CHANNEL_ID
 
 app.event('app_home_opened', ({ event }) => {
   console.log('app_home_opened', event);
@@ -40,8 +40,13 @@ app.event('channel_unarchive', async ({ event, context }) => {
 app.event('channel_created', ({ event }) => {
   console.log('channel_created', event);
 });
-app.event('channel_deleted', ({ event }) => {
+app.event('channel_deleted', async ({ event, context }) => {
   console.log('channel_deleted', event);
+  await app.client.chat.postMessage({
+    token: context.botToken,
+    channel: OWNER_CHANNEL_ID,
+    text: `<#${event.channel}> が消えたようだのぉ`,
+  });
 });
 app.event('channel_left', ({ event }) => {
   console.log('channel_left', event);
@@ -54,11 +59,21 @@ app.event('channel_rename', async ({ event, context }) => {
     text: `どこかで名が *${event.channel.name}* になったチャンネルがおるのぉ`,
   });
 });
-app.event('channel_shared', ({ event }) => {
+app.event('channel_shared', async ({ event, context }) => {
   console.log('channel_shared', event);
+  await app.client.chat.postMessage({
+    token: context.botToken,
+    channel: OWNER_CHANNEL_ID,
+    text: `<#${event.channel}> がどこかのチームと共有されたようだのぉ`,
+  });
 });
-app.event('channel_unshared', ({ event }) => {
+app.event('channel_unshared', async ({ event, context }) => {
   console.log('channel_unshared', event);
+  await app.client.chat.postMessage({
+    token: context.botToken,
+    channel: OWNER_CHANNEL_ID,
+    text: `<#${event.channel}> が他チームとの共有をとめたようだのぉ`,
+  });
 });
 
 app.error((error) => {
